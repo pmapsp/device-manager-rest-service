3e9c6162ae27080d05aca09f18ca1fc0120b2221
Pedro Miguel Pereira <pmapsp@gmail.com>
2022-03-18 22:45:27 +0000

    Added device class, new dependencies and repository interface
    Added Device class
    Added JPA,H2 and WEB dependencies
    Added Device repository interface

---
+++ src/main/java/devicemanagerrestservice/Device.java
@@ -0,0 +1,48 @@
+package devicemanagerrestservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Device {
+	
+	private @Id @GeneratedValue Long id;
+	private String brand;
+	private String name;
+	private Long creationTime;
+
+	Device(String name, String brand){
+		this.name = name;
+		this.brand = brand;
+		creationTime = System.currentTimeMillis();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public Long getCreationTime() {
+		return creationTime;
+	}	
+}
---
+++ src/main/java/devicemanagerrestservice/DeviceRepository.java
@@ -0,0 +1,7 @@
+package devicemanagerrestservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeviceRepository extends JpaRepository<Device, Long>{
+
+}
